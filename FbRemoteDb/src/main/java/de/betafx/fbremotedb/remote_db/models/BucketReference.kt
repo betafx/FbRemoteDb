7a5ff5d725ff95221249e66b55aac1b8524cb419
Fabian J <fabian.jung@mailbox.org>
2021-12-27 17:53:12 +0100

    Added Keep annotation

--- FbRemoteDb/src/main/java/de/betafx/fbremotedb/remote_db/models/BucketReference.kt
+++ FbRemoteDb/src/main/java/de/betafx/fbremotedb/remote_db/models/BucketReference.kt
@@ -1,10 +1,13 @@
 package de.betafx.fbremotedb.remote_db.models
 
+import androidx.annotation.Keep
+
 internal data class BucketReference(
     val name: String,
     val password: String
 ) {
 
+    @Keep
     constructor() : this("", "")
 
     fun toMutableMap() = mapOf(
